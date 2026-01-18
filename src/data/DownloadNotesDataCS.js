baca5e3d30204d64253404388e8e841247697ad7
UsamaShafique218 <usamashafique218@gmail.com>
2026-01-18 19:04:53 +0500

    File download issue solve

--- src/data/DownloadNotesDataCS.js
+++ src/data/DownloadNotesDataCS.js
@@ -3,32 +3,9 @@ const DownloadNotesDataCS = [
     id: 1,
     title: "CS202 Final Term Notes",
     description: "CS202 final term notes — download and enjoy your study!",
-    pdfUrl: "https://drive.google.com/drive/folders/13qJj43CGXwI4JbuZnsPliW-8EXH_x6hJ",
+    pdfUrl: "https://drive.google.com/file/d/1Htlj1NeLZwwvA1ePD0eyHYOHJ668qdgk/view?usp=sharing",
   },
-  {
-    id: 2,
-    title: "Quizzes",
-    description: "Quizzes help evaluate student knowledge quickly.",
-    pdfUrl: "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_2",
-  },
-  {
-    id: 3,
-    title: "Midterm Papers",
-    description: "Midterm papers assess students' understanding of the first half of the term.",
-    pdfUrl: "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_3",
-  },
-  {
-    id: 4,
-    title: "Final Papers",
-    description: "Final papers evaluate overall course comprehension and critical thinking.",
-    pdfUrl: "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_4",
-  },
-  {
-    id: 5,
-    title: "Lecture Notes",
-    description: "Lecture notes provide summarized explanations of key topics discussed in class.",
-    pdfUrl: "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_5",
-  },
+   
 ];
 
 export default DownloadNotesDataCS;
